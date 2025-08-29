370c01be5c8b619985975967e8ece2a0b32d041a
bwbl <theophile2006@gmail.com>
2025-08-29 11:01:14 +0200

    feat() Ce commit ajoute la première version du snail qui n'utilise pas de poo

---
+++ personnel/snail/snail/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace snail
+{
+
+    
+
+    internal class Program
+    {
+        static void Main()
+        {
+            int life = 0;
+            while (life <= 50) {
+                Console.SetCursorPosition(life, 0);
+                Console.Write();
+                System.Threading.Thread.Sleep(1000);
+                Console.SetCursorPosition(life, 0);
+                Console.Write("   ");
+                life++;
+            }
+        }
+
+        
+    }
+}
